0e4b29a2c2fccb17d4d00ac8ba79c3618a391da9
irvinbudwal01 <irvinb35@gmail.com>
2024-11-16 20:47:37 -0800

    attempt random/flow implementation
    converting back to udp to figure out easier architecture to identify potential solutions

--- ns.py-main/ML_Load_Balancer/fail_simulator1.py
+++ ns.py-main/ML_Load_Balancer/fail_simulator1.py
@@ -172,75 +172,11 @@ wire3_upstream.out = sender3
 wire4_upstream.out = sender4
 wire5_upstream.out = sender5
 
-env.run(until=100) #*******************************TO FIGURE OUT: IT SEEMS FLOW DEMUX LETS YOU DECIDE WHICH OUTPUTS GO TO WHICH SINK. RANDOM DEMUX RANDOMIZES AMONG ALL PORTS?
-                   #IT NEEDS TO ONLY RANDOMIZE ON OUTPUTS THAT GO TO SINKS. TO SIMPLIFY THIS PROCESS, CONVERT NETWORK BACK TO UDP INSTEAD OF TCP. FIND SOLUTION FIRST, THEN SWITCH
-                   #BACK TO TCP IF POSSIBLE.
-
-"""
-A basic example that connects two packet generators to a network wire with
-a propagation delay distribution, and then to a packet sink.
-"""
-
-from functools import partial
-import random
-from random import expovariate
-
-import simpy
-from ns.packet.dist_generator import DistPacketGenerator
-from ns.packet.sink import PacketSink
-from ns.port.wire import Wire
-
-
-def arrival_1():
-    """Packets arrive with a constant interval of 1.5 seconds."""
-    return 1.5
-
-
-def arrival_2():
-    """Packets arrive with a constant interval of 2.0 seconds."""
-    return 2.0
-
-
-def delay_dist():
-    return 0.1
-
-
-def packet_size():
-    return int(expovariate(0.01))
-
-
-env = simpy.Environment()
-
-ps = PacketSink(env, rec_flow_ids=False, debug=True)
-
-pg1 = DistPacketGenerator(env, "flow_1", arrival_1, packet_size, flow_id=0)
-pg2 = DistPacketGenerator(env, "flow_2", arrival_2, packet_size, flow_id=1)
-
-wire1 = Wire(env, partial(random.gauss, 0.1, 0.02), wire_id=1, debug=False)
-wire2 = Wire(env, delay_dist, wire_id=2, debug=False)
-
-pg1.out = wire1
-pg2.out = wire2
-wire1.out = ps
-wire2.out = ps
-
 env.run(until=100)
 
-print(
-    "Flow 1 packet delays: "
-    + ", ".join(["{:.2f}".format(x) for x in ps.waits["flow_1"]])
-)
-print(
-    "Flow 2 packet delays: "
-    + ", ".join(["{:.2f}".format(x) for x in ps.waits["flow_2"]])
-)
+# source_rate = 4600
 
-print(
-    "Packet arrival times in flow 1: "
-    + ", ".join(["{:.2f}".format(x) for x in ps.arrivals["flow_1"]])
-)
-
-print(
-    "Packet arrival times in flow 2: "
-    + ", ".join(["{:.2f}".format(x) for x in ps.arrivals["flow_2"]])
-)
+# wfq_server = WFQServer(env, source_rate, [1, 2], debug=True)
+# monitor = ServerMonitor(
+#     env, wfq_server, partial(expovariate, 0.1), pkt_in_service_included=True
+# )
